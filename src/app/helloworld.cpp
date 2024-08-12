6cc865afd0fd5879ffa56e2b2e0677d5d7438124
ASheikhm <afsanehsheikhmiri@gmail.com>
2024-08-12 17:55:58 -0400

    removing cmakeCache

--- src/app/helloworld.cpp
+++ src/app/helloworld.cpp
@@ -1,1 +1,13 @@
 #include <QApplication>
+#include <QLabel>
+
+int main (int argc, char* argv[])
+{
+    QApplication app (argc, argv);
+    QLabel* label = new QLabel("Hello World", 0);
+    label->resize(120, 30);
+
+    label->show();
+
+    app.exec();
+}
